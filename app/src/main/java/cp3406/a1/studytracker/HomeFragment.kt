22ae431ffccad66e4c509ed6959e92f7873751e4
danniellejones <dannielle.jones1@my.jcu.edu.au>
2023-04-07 02:35:32 +1000

    Customise settings to show default time

--- app/src/main/java/cp3406/a1/studytracker/HomeFragment.kt
+++ app/src/main/java/cp3406/a1/studytracker/HomeFragment.kt
@@ -1,15 +1,21 @@
 package cp3406.a1.studytracker
 
+import android.content.Context
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
+import androidx.preference.PreferenceManager
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import android.os.Build
+import android.content.SharedPreferences
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+//import cp3406.a1.studytracker.databinding.FragmentHomeBinding
+
+//import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * A simple [Fragment] subclass.
@@ -17,26 +23,40 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+//        val binding = DataBindingUtil.inflate<FragmentHomeBinding>(inflater, R.layout.fragment_home, container, false)
+//        val binding: FragmentHomeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
         // Inflate the layout for this fragment
         setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_home, container, false)
+//        return binding.root
     }
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        val navController = this.findNavController(R.id.mainFragment)
+//        NavigationUI.setupActionBarWithNavController(this, navController)
+//    }
+
+
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        fab_settings.setOnClickListener {
+//            findNavController().navigate(R.id.action_homeFragment_to_settingsFragment)
+//        }
+//
+//        loadSettings()
+//    }
+//
+//    private fun loadSettings() {
+//        val sp = PreferenceManager.getDefaultSharedPreferences(context)
+//        val defaultTime = sp.getString("default_time_key", "")
+//
+//        default_time.text = "Default time: $defaultTime"
+//    }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
--- app/src/main/java/cp3406/a1/studytracker/MainActivity.kt
+++ app/src/main/java/cp3406/a1/studytracker/MainActivity.kt
@@ -5,20 +5,26 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
-//import cp3406.a1.studytracker.android.navigation.databinding.ActivityMainBinding
+import androidx.navigation.ui.setupActionBarWithNavController
+import cp3406.a1.studytracker.databinding.ActivityMainBinding
+
+//import cp3406.a1.studytracker.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val binding = DataBindingUtil.setContentView<ActivityMainBuilding>(this, R.layout.activity_main)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+//        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
-//        val navController = this.findNavController(R.id.mainFragment)
-//        NavigationUI.setupActionBarWithNavController(this,navController)
+//        setupActionBarWithNavController(findNavController(R.id.mainFragment))
+
+//        val navController = findNavController(R.id.mainFragment)
+//        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
 //    override fun onSupportNavigateUp(): Boolean {
-//        val navController = this.findNavController(R.id.mainFragment)
-//        return navController.navigateUp()
+//        val navController = findNavController(R.id.mainFragment)
+//        return navController.navigateUp() || super.onSupportNavigateUp()
 //    }
 }
