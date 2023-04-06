374bd8f2a8c4aea9670880573b58b850af5aa5bc
danniellejones <dannielle.jones1@my.jcu.edu.au>
2023-04-06 15:55:20 +1000

    Initial commit

---
+++ app/src/main/java/cp3406/a1/studytracker/MainActivity.kt
@@ -0,0 +1,11 @@
+package cp3406.a1.studytracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
