4dfdfd38fb334b68c8bfe7417a3618f59436ad9b
danniellejones <dannielle.jones1@my.jcu.edu.au>
2023-04-07 00:04:28 +1000

    Add home and settings fragment navigation

--- app/src/main/java/cp3406/a1/studytracker/MainActivity.kt
+++ app/src/main/java/cp3406/a1/studytracker/MainActivity.kt
@@ -2,10 +2,23 @@ package cp3406.a1.studytracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+//import cp3406.a1.studytracker.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        val binding = DataBindingUtil.setContentView<ActivityMainBuilding>(this, R.layout.activity_main)
         setContentView(R.layout.activity_main)
+
+//        val navController = this.findNavController(R.id.mainFragment)
+//        NavigationUI.setupActionBarWithNavController(this,navController)
     }
+
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = this.findNavController(R.id.mainFragment)
+//        return navController.navigateUp()
+//    }
 }
