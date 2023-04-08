8da05b2dc3f2fbb519ef7e1650b9761b8ddc3628
danniellejones <dannielle.jones1@my.jcu.edu.au>
2023-04-09 02:31:30 +1000

    Add docstrings and inline comments

--- app/src/main/java/cp3406/a1/studytracker/SettingsFragment.kt
+++ app/src/main/java/cp3406/a1/studytracker/SettingsFragment.kt
@@ -4,60 +4,15 @@
 
 package cp3406.a1.studytracker
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import androidx.preference.PreferenceFragmentCompat
 
+/** Set up settings fragment and link to root preferences */
 class SettingsFragment : PreferenceFragmentCompat() {
 
+    /** Use sharedPreferences and link to root preferences to create settings */
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
     }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        Log.i("SettingsFragment", "onAttach called")
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.i("SettingsFragment", "onCreate called")
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        Log.i("SettingsFragment", "onViewCreated called")
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.i("SettingsFragment", "onStart called")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.i("SettingsFragment", "onResume called")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.i("SettingsFragment", "onPause called")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.i("SettingsFragment", "onStop called")
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        Log.i("SettingsFragment", "onDestroyView called")
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        Log.i("SettingsFragment", "onDetach called")
-    }
 }
+
